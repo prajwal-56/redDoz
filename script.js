25f233550db7aa8dcd3814bbee5fce84307d86f4
prajwal-56 <prajwalpraveen2006@gmail.com>
2026-02-02 23:19:01 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let foo = document.getElementById("redditUsername").value
+
+
+console.log("hi mom!")
+console.log("The user wants to find posts and comments from the user "+ foo)
+
+function generate_link(){
+    let foo = document.getElementById("redditUsername").value
+
+    console.log("here " + foo)
+
+    const prefix = "site:reddit.com"
+
+    // const thatUrl = 
+}
